318eda34c8476fafc47d7aff9801d6565b5da6e7
Darcy Phipps <diphipps@ucsc.edu>
2020-05-15 02:18:48 -0700

    Added helper functions and more complete movement

--- src/prefabs/Unit.js
+++ src/prefabs/Unit.js
@@ -21,16 +21,20 @@ class Unit extends Phaser.GameObjects.Sprite{
             return;
         //If the target is right next to us just go there
         if(this.scene.checkAdjacency(this.tile, target)){
-            this.remainingMovement -= target.movementCost;
-            this.tile = target;
-            this.x = this.scene.map.tileToWorldX(target.x);
-            this.y = this.scene.map.tileToWorldY(target.y);
+            
+            this.moveTo(target)
         }
         //Otherwise, do the pathfinding thing
         else{
             //First just run A* and get the path to the place
             //Passing in the tileMap as a param because it's just shorter
             let path = AStar(target, this.scene.map);
+            //Keep moving until we can't any more
+            while(this.remainingMovement > 0 && path.length != 0){
+                let nextTile = path.shift();
+                this.moveTo(nextTile);
+            }
+            //We should now be as close to the target as we could have gotten
         }
 
         //A* pathfinding algorithm
@@ -121,8 +125,23 @@ class Unit extends Phaser.GameObjects.Sprite{
         let current = destination
         while(steps.has(destination)){
             current = steps.get(current)
-            route.push(current)
+            route.unshift(current)
         }
         return route;
+    }
+
+    //Helper function for moving tiles
+    changeTile(destination){
+        this.tile = destination;
+        this.x = this.scene.map.tileToWorldX(destination.x);
+        this.y = this.scene.map.tileToWorldY(destination.y);
+    }
+
+    //Another helper function for moving
+    //This one is separate in case we want to use the above for moving without paying cost
+    moveTo(destination){
+        this.changeTile(destination);
+        this.remainingMovement -= destination.movementCost;
+        this.remainingMovement = Math.max(0, this.remainingMovement);
     }
 }
