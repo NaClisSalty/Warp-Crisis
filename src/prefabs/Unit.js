2c0de99f9f959aa96fc9ebe1c5bb5bba59eca442
Darcy Phipps <diphipps@ucsc.edu>
2020-05-15 00:02:19 -0700

    Added basic unit constructor

--- src/prefabs/Unit.js
+++ src/prefabs/Unit.js
@@ -1,5 +1,6 @@
 class Unit extends Phaser.GameObjects.Sprite{
-    constructor(scene, x, y, texture, frame){
+    constructor(scene, x, y, texture, frame, movement){
         super(scene, x, y, texture, frame);
+        this.movement = movement;
     }
 }
