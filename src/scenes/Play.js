ec0d18a49d166983eb0c58405e960004c87fcb71
Darcy Phipps <diphipps@ucsc.edu>
2020-05-15 02:10:13 -0700

    Implemented movement algorithm mostly

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -21,4 +21,8 @@ class Play extends Phaser.Scene {
         this.paused = true;
         console.log("Bye!")
     }
+    //Quick and dirty method to check if 2 tiles are adjacent.
+    checkAdjacency(tile, otherTile){
+        return(Math.abs(tile.x-otherTile.x)<= 1 && Math.abs(tile.y-otherTile.y)<= 1);
+    }
 }
