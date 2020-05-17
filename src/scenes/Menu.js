fecf83563a5311bb39584800c8c846a93120cfbb
Ian Monahan <immonaha@ucsc.edu>
2020-05-17 15:26:27 -0700

    added button to menu

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -7,10 +7,32 @@ class Menu extends Phaser.Scene {
     }
 
     create() {
-
+        let centerX = game.config.width/2;
+        let centerY = game.config.height/2;
+        this.clickButton = this.add.text(centerX-20, centerY, 'Play', {fill: '#d437bc'})
+        .setInteractive({ useHandCursor: true })
+        .on('pointerover', () => this.enterButtonHoverState())
+        .on('pointerout', () => this.enterButtonRestState())
+        .on('pointerdown', () => this.enterButtonActiveState())
+        .on('pointerup', () => {
+            this.enterButtonHoverState();
+            this.scene.start("playScene");
+        });
     }
 
     update() {  
 
+    }
+
+    enterButtonHoverState() {
+        this.clickButton.setStyle({ fill: '#6b1c5f'});
+    }
+
+    enterButtonRestState() {
+        this.clickButton.setStyle({ fill: '#6b1c5f' });
+    }
+
+    enterButtonActiveState() {
+        this.clickButton.setStyle({ fill: '#000000' });
     }
 }
