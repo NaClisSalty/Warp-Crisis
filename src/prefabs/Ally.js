56f1ae942c9cdd966bda0dddab62bcc4586a11c9
Ian Monahan <immonaha@ucsc.edu>
2020-05-17 21:48:57 -0700

    units are selectable

--- src/prefabs/Ally.js
+++ src/prefabs/Ally.js
@@ -2,6 +2,8 @@ class Ally extends Unit{
     constructor(scene, x, y, texture, frame, movement, tile, strength, health){
         super(scene, x, y, texture, frame, movement, tile, strength, health);
 
-        this.on('pointerDown', ()=>this.scene.selected= this);
+        this.on('pointerdown', function(pointer){
+            scene.selected = this;
+        });
     }
 }
