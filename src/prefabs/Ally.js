15454a43f4f8047db77a3619303c144a0f7edcbc
Ian Monahan <immonaha@ucsc.edu>
2020-05-17 21:53:29 -0700

    fixed names

--- src/prefabs/Ally.js
+++ src/prefabs/Ally.js
@@ -1,7 +1,7 @@
 class Ally extends Unit{
     constructor(scene, x, y, texture, frame, movement, tile, strength, health){
         super(scene, x, y, texture, frame, movement, tile, strength, health);
-
+        this.name = texture.slice(4);
         this.on('pointerdown', function(pointer){
             scene.selected = this;
         });
--- src/prefabs/Enemy.js
+++ src/prefabs/Enemy.js
@@ -1,5 +1,6 @@
 class Enemy extends Unit{
     constructor(scene, x, y, texture, frame, movement, tile, strength, health){
         super(scene, x, y, texture, frame, movement, tile, strength, health);
+        this.name = texture.slice(5);
     }
 }
